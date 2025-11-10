454df3c57468a1df24b71da20e694abc433e05d6
Timothé MEDICO <medico.timothe@gmail.com>
2025-11-10 19:33:48 +0200

    feat: Step 2 - Task Id

--- src/Models/TaskItem.cs
+++ src/Models/TaskItem.cs
@@ -1,7 +1,10 @@
+using System;
+
 namespace TodoListApp.Models;
 
 public class TaskItem
 {
     public string Title { get; set; } = string.Empty;
     public bool IsCompleted { get; set; } = false;
+    public Guid Id { get; } = Guid.NewGuid();
 }
