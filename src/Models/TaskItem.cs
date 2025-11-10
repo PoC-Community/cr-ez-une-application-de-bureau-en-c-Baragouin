b4977505e926ee442b436e318e606229046ad534
Timothé MEDICO <medico.timothe@gmail.com>
2025-11-10 21:55:02 +0200

    refacto: Global refactoring

--- src/Models/TaskItem.cs
+++ src/Models/TaskItem.cs
@@ -5,8 +5,7 @@ namespace TodoListApp.Models;
 
 public class TaskItem
 {
-    public Guid Id { get; } = Guid.NewGuid();
     public string Title { get; set; } = string.Empty;
-    public bool IsCompleted { get; set; } = false;
-    public List<string> Tags { get; set; } = new();
+    public bool IsCompleted { get; set; }
+    public List<string> Tags { get; set; } = [];
 }
